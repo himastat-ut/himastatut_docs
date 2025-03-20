ff0a4f8d1a8a5fb107f2a47e4768aee2fde07b14
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2025-03-20 19:54:02 +0700

    Verify rights to https://himastatut.my.id yandex

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -11,6 +11,7 @@ export default defineUserConfig({
   head: [
     ['meta', { name: 'algolia-site-verification', content: '70561EEFC0C70B02' }],
     ['meta', { name: 'msvalidate.01', content: '4077B11CAE89D53B7DAE1D82710447B0' }],
+    ['meta', { name: 'yandex-verification', content: '1275725ccdfb310d' }],
     ['link', { rel: 'icon', href: '/assets/image/logo.png' }]
   ],
   theme,
